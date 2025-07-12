594ae86b5db8729d918aec9200ab14e6a4679e83
Muhammad Irfan <m.irfandeni@gmail.com>
2025-07-13 00:20:21 +0800

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    internal class Calculator
+    {
+        public int Addition(int a, int b) => a + b;
+        public int Subtraction(int a, int b) => a - b;
+        public double Multiplication(double a, double b) => a * b;
+        public double Division(double a, double b) => a / b;
+    }
+}
